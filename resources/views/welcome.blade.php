9b0bb3973ebf8817cb0eb902a5e4013463529399
williamtruppi <williamt@hotmail.it>
2021-03-03 17:37:08 +0100

    navbar bottom GUEST completata

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,6 +30,45 @@
                     </ul>
                 </div>
                 {{-- apertura navbar_top --}}
+
+                {{-- apertura navbar_bottom --}}
+                <div class="navbar_bottom">
+
+                    {{-- apertura container --}}
+                    <div class="container d-flex">
+
+                        {{-- apertura navbar_bottom_left --}}
+                        <div class="navbar_bottom_left">
+                            <img src="{{asset('img/logo.png')}}" alt="">
+                        </div>
+                        {{-- apertura navbar_bottom_left --}}
+
+                        {{-- apertura navbar_bottom_center --}}
+                        <div class="navbar_bottom_center d-flex">
+                            <ul class="d-flex">
+                                <li><a href="#">CHARACTER</a></li>
+                                <li><a href="#">COMICS</a></li>
+                                <li><a href="#">MOVIES</a></li>
+                                <li><a href="#">TV</a></li>
+                                <li><a href="#">GAMES</a></li>
+                                <li><a href="#">VIDEOS</a></li>
+                                <li><a href="#">NEWS</a></li>
+                                <li><a href="#">SHOP</a></li>
+                            </ul>
+                        </div>
+                        {{-- apertura navbar_bottom_center --}}
+
+                        {{-- apertura navbar_bottom_right --}}
+                        <div class="navbar_bottom_right d-flex">
+                            <input type="text">
+                        </div>
+                        {{-- apertura navbar_bottom_right --}}
+
+                    </div>
+                    {{-- chiusura container --}}
+
+                </div>
+                {{-- chiusura navbar_bottom --}}
             </nav>
             {{-- chiusura nav --}}
         </header>

c0f74f42471c969c09471cd14f8ea955825b41b4
williamtruppi <williamt@hotmail.it>
2021-03-03 16:00:20 +0100

    creato HomeController per l'admin

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('auth')->namespace('Admin')->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', 'HomeController@index')->name('index');
+    Route::resource('posts', 'PostController');
+});

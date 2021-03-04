971d96773a7f2f4180b22274a9e14103682c96cc
williamtruppi <williamt@hotmail.it>
2021-03-04 10:20:55 +0100

    correzione aggiunta colonna cover

--- database/migrations/2021_03_04_090231_add_cover_to_comics_table.php
+++ database/migrations/2021_03_04_090231_add_cover_to_comics_table.php
@@ -14,7 +14,7 @@ class AddCoverToComicsTable extends Migration
     public function up()
     {
         Schema::table('comics', function (Blueprint $table) {
-            //
+            $table->string('cover')->after('slug')->nullable();
         });
     }
 
